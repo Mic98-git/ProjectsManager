b31196b5bc3ea23d12b322ddef19371bd269d194
Daniele <fippidaniele@gmail.com>
2020-05-31 17:00:45 +0200

    Other two classes with attrs

---
+++ src/main/java/it/uniroma3/siw/sping/model/Task.java
@@ -0,0 +1,14 @@
+package it.uniroma3.siw.sping.model;
+
+import java.time.LocalDateTime;
+
+public class Task {
+	
+	private Long id;
+	private String name;
+	private String description;
+	private LocalDateTime creationDate;
+
+	
+
+}
---
+++ src/main/java/it/uniroma3/siw/sping/model/User.java
@@ -0,0 +1,19 @@
+package it.uniroma3.siw.sping.model;
+
+import java.time.LocalDateTime;
+
+public class User {
+	
+	private Long id;
+
+	private String name;
+	private String lastname;
+	
+	private LocalDateTime creationDate;
+	
+	private String username;
+	private String password;
+		
+		
+	
+}
