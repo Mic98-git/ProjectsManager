55434fbe19c463f2707ca902a63b4dc8ffea685b
Daniele <fippidaniele@gmail.com>
2020-05-31 17:37:56 +0200

    annotations and relationships added

--- src/main/java/it/uniroma3/siw/sping/model/Task.java
+++ src/main/java/it/uniroma3/siw/sping/model/Task.java
@@ -1,14 +1,29 @@
 package it.uniroma3.siw.sping.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 public class Task {
 	
+	@Id
 	private Long id;
 	private String name;
 	private String description;
-	private LocalDateTime creationDate;
+	private LocalDateTime creationDate = LocalDateTime.now();
 
+	@ManyToOne
+	private Project project;
+	@ManyToMany
+	private List<Tag> tags = new ArrayList<>();
+	@OneToMany
+	private List<Comment> comments = new ArrayList<>();
+	
 	
 
 }
--- src/main/java/it/uniroma3/siw/sping/model/User.java
+++ src/main/java/it/uniroma3/siw/sping/model/User.java
@@ -1,19 +1,36 @@
 package it.uniroma3.siw.sping.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 public class User {
 	
+	@Id
 	private Long id;
 
 	private String name;
 	private String lastname;
 	
-	private LocalDateTime creationDate;
+	private LocalDateTime creationDate = LocalDateTime.now();
 	
 	private String username;
 	private String password;
-		
-		
+	
+	@ManyToOne // FIXME o one to one?
+	private Role role;
+	
+	@ManyToMany
+	private List<Project> visibleProjects = new ArrayList<>();
+	
+	@OneToMany
+	private List<Project> projects = new ArrayList<>();
+	
+	
 	
 }
