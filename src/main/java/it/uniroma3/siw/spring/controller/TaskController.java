b5bda40ed90186442456dfac919d1651bba854ef
phi180 <daniele@debianpc.homenet.telecomitalia.it>
2020-06-17 22:34:24 +0200

    altri validator sistemati

--- src/main/java/it/uniroma3/siw/spring/controller/TaskController.java
+++ src/main/java/it/uniroma3/siw/spring/controller/TaskController.java
@@ -2,6 +2,8 @@ package it.uniroma3.siw.spring.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,6 +27,7 @@ import it.uniroma3.siw.spring.model.Task;
 import it.uniroma3.siw.spring.service.ProjectService;
 import it.uniroma3.siw.spring.service.TagService;
 import it.uniroma3.siw.spring.service.TaskService;
+import it.uniroma3.siw.spring.validator.TaskValidator;
 import net.bytebuddy.asm.Advice.This;
 
 @Controller
@@ -42,10 +45,13 @@ public class TaskController {
 	@Autowired
 	SessionData sessionData;
 	
+	@Autowired
+	private TaskValidator taskValidator;
+	
 	/* projectId non servirebbe, ma lo metto per convenienza */
 	@RequestMapping(value = {"/projects/{projectId}/task/{taskId}"}, method = RequestMethod.GET)
-	public String viewTaskDetails(@PathVariable Long projectId,@PathVariable Long taskId,
-			
+	public String viewTaskDetails(@PathVariable Long projectId,
+			@PathVariable Long taskId,			
 			Model model) {
 		Task task = this.taskService.getTask(taskId);
 		model.addAttribute("task",task);
@@ -102,18 +108,29 @@ public class TaskController {
 		// serve per la pagina, voglio informazioni sul progetto
 		model.addAttribute("project",project);
 		// per il form del task nuovo
-		model.addAttribute("task",new Task());
-		//System.out.print("");
+		model.addAttribute("taskForm",new Task());
+		System.out.print("");
 		
 		return "newTask";
 	}
 	
 	@PostMapping("/projects/{projectId}/task/new")
-	public String addNewTask(@ModelAttribute("task") Task task,
+	public String addNewTask(@Valid @ModelAttribute("taskForm") Task task,
 			BindingResult taskBindingResult,
-			@PathVariable Long projectId) {
+			@PathVariable Long projectId,
+			Model model) {
 		Project project = projectService.getProject(projectId);
-		//System.out.println();
+		if(!project.getMembers().contains(sessionData.getLoggedUser())) {
+			return "noAuth";
+		}
+		
+		taskValidator.validate(task, taskBindingResult);
+		if(taskBindingResult.hasErrors()) {// serve per la pagina, voglio informazioni sul progetto
+			model.addAttribute("project",project);
+			return "newTask";
+		}
+		
+		System.out.println();
 		project.getProjectTasks().add(task);
 		projectService.saveProject(project);
 		
@@ -135,7 +152,15 @@ public class TaskController {
 	@PostMapping("/projects/{projectId}/task/{taskId}/edit")
 	public String updateTask(@PathVariable Long projectId,
 			@PathVariable Long taskId,
-			@ModelAttribute Task task) {
+			@Valid @ModelAttribute("taskForm") Task task,
+			BindingResult taskBindingResult,
+			Model model) {
+		this.taskValidator.validate(task, taskBindingResult);
+		if(taskBindingResult.hasErrors()) {
+			model.addAttribute("projectId", projectId);	
+			return "editTask";
+		}
+		
 		Task t = taskService.getTask(taskId);
 		t.setName(task.getName());
 		t.setDescription(task.getDescription());
@@ -147,7 +172,8 @@ public class TaskController {
 	}
 	
 	@PostMapping("/projects/{projectId}/task/{taskId}/completed")
-	public String markCompletedTask(@PathVariable Long projectId, @PathVariable Long taskId) {
+	public String markCompletedTask(@PathVariable Long projectId, 
+			@PathVariable Long taskId) {
 		Task task = taskService.getTask(taskId);
 		task.setCompleted(true);
 		taskService.saveTask(task);
